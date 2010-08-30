16689e03a7b6738ade28c4bd8be91241070ec79b
alexnewski <alexnewski@2e5c6a54-fd55-4b07-886d-3f0f25a3bc6a>
2010-08-30 00:23:11 +0000

    TIGRESS

---
+++ sgl/BD/DATA.sql
@@ -0,0 +1,28 @@
+-- ----------------------------
+-- Records of proceso
+-- ----------------------------
+INSERT INTO `proceso` VALUES ('1', 'P1', '3');
+INSERT INTO `proceso` VALUES ('2', 'P2', '4');
+-- ----------------------------
+-- Records of fase
+-- ----------------------------
+INSERT INTO `fase` VALUES ('1', 'F1', null, 0x41, '1');
+INSERT INTO `fase` VALUES ('2', 'F2', null, 0x42, '1');
+INSERT INTO `fase` VALUES ('3', 'F3', null, 0x43, '1');
+INSERT INTO `fase` VALUES ('4', 'F1', null, 0x44, '2');
+
+-- ----------------------------
+-- Records of empresa
+-- ----------------------------
+INSERT INTO `empresa` VALUES ('1', 'POLAR', '333', 'LOS RUICES', '', '', '', '');
+
+-- ----------------------------
+-- Records of pais
+-- ----------------------------
+INSERT INTO `pais` VALUES ('1', 'BRASIL');
+
+-- ---------------------------
+-- Records of proveedor
+-- ----------------------------
+INSERT INTO `proveedor` VALUES ('1', 'BRASILEAN', 'SAO PAULO', '', '', '1');
+
