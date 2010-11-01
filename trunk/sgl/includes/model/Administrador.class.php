d9197216749dc9dcfef74ac86fff60b8bb52d940
ayusopatricia <ayusopatricia@2e5c6a54-fd55-4b07-886d-3f0f25a3bc6a>
2010-11-01 17:26:06 +0000

    Cambios semana 18 - 24 de octubre

--- trunk/sgl/includes/model/Administrador.class.php
+++ trunk/sgl/includes/model/Administrador.class.php
@@ -1,151 +1,161 @@
 <?php
-	require(__MODEL_GEN__ . '/AdministradorGen.class.php');
 
-	/**
-	 * The Administrador class defined here contains any
-	 * customized code for the Administrador class in the
-	 * Object Relational Model.  It represents the "ADMINISTRADOR" table 
-	 * in the database, and extends from the code generated abstract AdministradorGen
-	 * class, which contains all the basic CRUD-type functionality as well as
-	 * basic methods to handle relationships and index-based loading.
-	 * 
-	 * @package My QCubed Application
-	 * @subpackage DataObjects
-	 * 
-	 */
-	class Administrador extends AdministradorGen {
-		/**
-		 * Default "to string" handler
-		 * Allows pages to _p()/echo()/print() this object, and to define the default
-		 * way this object would be outputted.
-		 *
-		 * Can also be called directly via $objAdministrador->__toString().
-		 *
-		 * @return string a nicely formatted string representation of this object
-		 */
-		public function __toString() {
-			return sprintf('Administrador Object %s',  $this->intIdADMINISTRADOR);
-		}
+require(__MODEL_GEN__ . '/AdministradorGen.class.php');
 
+/**
+ * The Administrador class defined here contains any
+ * customized code for the Administrador class in the
+ * Object Relational Model.  It represents the "ADMINISTRADOR" table 
+ * in the database, and extends from the code generated abstract AdministradorGen
+ * class, which contains all the basic CRUD-type functionality as well as
+ * basic methods to handle relationships and index-based loading.
+ * 
+ * @package My QCubed Application
+ * @subpackage DataObjects
+ * 
+ */
+class Administrador extends AdministradorGen {
 
-		// Override or Create New Load/Count methods
-		// (For obvious reasons, these methods are commented out...
-		// but feel free to use these as a starting point)
-/*
-		public static function LoadArrayBySample($strParam1, $intParam2, $objOptionalClauses = null) {
-			// This will return an array of Administrador objects
-			return Administrador::QueryArray(
-				QQ::AndCondition(
-					QQ::Equal(QQN::Administrador()->Param1, $strParam1),
-					QQ::GreaterThan(QQN::Administrador()->Param2, $intParam2)
-				),
-				$objOptionalClauses
-			);
-		}
+    /**
+     * Default "to string" handler
+     * Allows pages to _p()/echo()/print() this object, and to define the default
+     * way this object would be outputted.
+     *
+     * Can also be called directly via $objAdministrador->__toString().
+     *
+     * @return string a nicely formatted string representation of this object
+     */
+    public function __toString() {
+        return sprintf('Administrador Object %s', $this->intIdADMINISTRADOR);
+    }
 
-		public static function LoadBySample($strParam1, $intParam2, $objOptionalClauses = null) {
-			// This will return a single Administrador object
-			return Administrador::QuerySingle(
-				QQ::AndCondition(
-					QQ::Equal(QQN::Administrador()->Param1, $strParam1),
-					QQ::GreaterThan(QQN::Administrador()->Param2, $intParam2)
-				),
-				$objOptionalClauses
-			);
-		}
+    public static function LoadByAmLog($login, $objOptionalClauses = null) {
+        return Administrador::QuerySingle(
+                QQ::AndCondition(
+                        QQ::Equal(QQN::Administrador()->Login, $login)
+                ),
+                $objOptionalClauses
+        );
+    }
 
-		public static function CountBySample($strParam1, $intParam2, $objOptionalClauses = null) {
-			// This will return a count of Administrador objects
-			return Administrador::QueryCount(
-				QQ::AndCondition(
-					QQ::Equal(QQN::Administrador()->Param1, $strParam1),
-					QQ::Equal(QQN::Administrador()->Param2, $intParam2)
-				),
-				$objOptionalClauses
-			);
-		}
+    // Override or Create New Load/Count methods
+    // (For obvious reasons, these methods are commented out...
+    // but feel free to use these as a starting point)
+    /*
+      public static function LoadArrayBySample($strParam1, $intParam2, $objOptionalClauses = null) {
+      // This will return an array of Administrador objects
+      return Administrador::QueryArray(
+      QQ::AndCondition(
+      QQ::Equal(QQN::Administrador()->Param1, $strParam1),
+      QQ::GreaterThan(QQN::Administrador()->Param2, $intParam2)
+      ),
+      $objOptionalClauses
+      );
+      }
 
-		public static function LoadArrayBySample($strParam1, $intParam2, $objOptionalClauses) {
-			// Performing the load manually (instead of using QCubed Query)
+      public static function LoadBySample($strParam1, $intParam2, $objOptionalClauses = null) {
+      // This will return a single Administrador object
+      return Administrador::QuerySingle(
+      QQ::AndCondition(
+      QQ::Equal(QQN::Administrador()->Param1, $strParam1),
+      QQ::GreaterThan(QQN::Administrador()->Param2, $intParam2)
+      ),
+      $objOptionalClauses
+      );
+      }
 
-			// Get the Database Object for this Class
-			$objDatabase = Administrador::GetDatabase();
+      public static function CountBySample($strParam1, $intParam2, $objOptionalClauses = null) {
+      // This will return a count of Administrador objects
+      return Administrador::QueryCount(
+      QQ::AndCondition(
+      QQ::Equal(QQN::Administrador()->Param1, $strParam1),
+      QQ::Equal(QQN::Administrador()->Param2, $intParam2)
+      ),
+      $objOptionalClauses
+      );
+      }
 
-			// Properly Escape All Input Parameters using Database->SqlVariable()
-			$strParam1 = $objDatabase->SqlVariable($strParam1);
-			$intParam2 = $objDatabase->SqlVariable($intParam2);
+      public static function LoadArrayBySample($strParam1, $intParam2, $objOptionalClauses) {
+      // Performing the load manually (instead of using QCubed Query)
 
-			// Setup the SQL Query
-			$strQuery = sprintf('
-				SELECT
-					`ADMINISTRADOR`.*
-				FROM
-					`ADMINISTRADOR` AS `ADMINISTRADOR`
-				WHERE
-					param_1 = %s AND
-					param_2 < %s',
-				$strParam1, $intParam2);
+      // Get the Database Object for this Class
+      $objDatabase = Administrador::GetDatabase();
 
-			// Perform the Query and Instantiate the Result
-			$objDbResult = $objDatabase->Query($strQuery);
-			return Administrador::InstantiateDbResult($objDbResult);
-		}
-*/
+      // Properly Escape All Input Parameters using Database->SqlVariable()
+      $strParam1 = $objDatabase->SqlVariable($strParam1);
+      $intParam2 = $objDatabase->SqlVariable($intParam2);
 
+      // Setup the SQL Query
+      $strQuery = sprintf('
+      SELECT
+      `ADMINISTRADOR`.*
+      FROM
+      `ADMINISTRADOR` AS `ADMINISTRADOR`
+      WHERE
+      param_1 = %s AND
+      param_2 < %s',
+      $strParam1, $intParam2);
 
-
-
-		// Override or Create New Properties and Variables
-		// For performance reasons, these variables and __set and __get override methods
-		// are commented out.  But if you wish to implement or override any
-		// of the data generated properties, please feel free to uncomment them.
-/*
-		protected $strSomeNewProperty;
-
-		public function __get($strName) {
-			switch ($strName) {
-				case 'SomeNewProperty': return $this->strSomeNewProperty;
-
-				default:
-					try {
-						return parent::__get($strName);
-					} catch (QCallerException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-			}
-		}
-
-		public function __set($strName, $mixValue) {
-			switch ($strName) {
-				case 'SomeNewProperty':
-					try {
-						return ($this->strSomeNewProperty = QType::Cast($mixValue, QType::String));
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				default:
-					try {
-						return (parent::__set($strName, $mixValue));
-					} catch (QCallerException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-			}
-		}
-*/
+      // Perform the Query and Instantiate the Result
+      $objDbResult = $objDatabase->Query($strQuery);
+      return Administrador::InstantiateDbResult($objDbResult);
+      }
+     */
 
 
 
-		// Initialize each property with default values from database definition
-/*		
-		public function __construct()
-		{
-			$this->Initialize();
-		}
-*/
 
-	}
+    // Override or Create New Properties and Variables
+    // For performance reasons, these variables and __set and __get override methods
+    // are commented out.  But if you wish to implement or override any
+    // of the data generated properties, please feel free to uncomment them.
+    /*
+      protected $strSomeNewProperty;
+
+      public function __get($strName) {
+      switch ($strName) {
+      case 'SomeNewProperty': return $this->strSomeNewProperty;
+
+      default:
+      try {
+      return parent::__get($strName);
+      } catch (QCallerException $objExc) {
+      $objExc->IncrementOffset();
+      throw $objExc;
+      }
+      }
+      }
+
+      public function __set($strName, $mixValue) {
+      switch ($strName) {
+      case 'SomeNewProperty':
+      try {
+      return ($this->strSomeNewProperty = QType::Cast($mixValue, QType::String));
+      } catch (QInvalidCastException $objExc) {
+      $objExc->IncrementOffset();
+      throw $objExc;
+      }
+
+      default:
+      try {
+      return (parent::__set($strName, $mixValue));
+      } catch (QCallerException $objExc) {
+      $objExc->IncrementOffset();
+      throw $objExc;
+      }
+      }
+      }
+     */
+
+
+
+    // Initialize each property with default values from database definition
+    /*
+      public function __construct()
+      {
+      $this->Initialize();
+      }
+     */
+}
+
 ?>
